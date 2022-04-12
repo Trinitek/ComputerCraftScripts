e38dbe2f69d17b382733d5c45f2c3abbb1afc4c2
Blake Burgess <trinitek@live.com>
2022-04-12 08:05:07 -0400

    Add fellTree script

---
+++ scripts/fellTree.lua
@@ -0,0 +1,50 @@
+
+-- Expected starting position is 1 block away from bottom left corner of 2x2 spruce.
+
+---@type integer
+local height = 0
+
+turtle.forward()
+turtle.dig()
+turtle.suck()
+turtle.forward()
+
+repeat
+    turtle.dig()
+    turtle.suck()
+    turtle.forward()
+
+    turtle.turnRight()
+
+    turtle.dig()
+    turtle.suck()
+    turtle.forward()
+
+    turtle.turnRight()
+
+    turtle.dig()
+    turtle.suck()
+    turtle.forward()
+
+    turtle.turnRight()
+    
+    turtle.forward()
+    
+    turtle.turnRight()
+
+    local hasBlockAbove = turtle.digUp()
+
+    if hasBlockAbove then
+        turtle.suck()
+        turtle.up()
+        height = height + 1
+    end
+until not hasBlockAbove
+
+for i = height, 1, -1 do
+    turtle.down()
+    turtle.back()
+    turtle.back()
+end
+
+print("Height " .. height)
